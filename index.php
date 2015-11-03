f16a26da06eb5c588155a6f710bc4ea9b070507b
Alexander Pobereznichenko <apobereznichenko@gmail.com>
2015-11-04 00:09:59 +0200

    The HttpFoundation Component

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php
 
-$input = $_GET['name'];
+require_once __DIR__ . '/vendor/autoload.php';
 
-printf("Hello, %s!!!", $input);
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+$request = Request::createFromGlobals();
+
+$name = $request->query->get('name', 'World');
+
+$responseText = sprintf('Hello, %s !!!', htmlspecialchars($name, ENT_QUOTES, 'UTF-8'));
+$response = new Response($responseText);
+
+$response->send();
